59224f654d25c76e2eb30507cc9f49d11f75d901
Aditi9243 <aditi10950@gmail.com>
2022-04-11 22:01:52 +0530

    copy

--- prerequisite/spread/object.js
+++ prerequisite/spread/object.js
@@ -10,9 +10,11 @@ let obj = {
 }
 
 //let obj1 = obj    //similar to arrays
-//let obj1 = {...obj}
+//let obj1 = {...obj}   //shallow copy*** -> not a efficient way to copy data as we need to spread at diff levels
 
-let obj1 = {...obj, address: {...obj.address, state : {...obj.address.state}}}
+//let obj1 = {...obj, address: {...obj.address, state : {...obj.address.state}}} 
+
+let obj1 = JSON.parse(JSON.stringify(obj)) //deep copy***
 
 obj.name = 'Mark'
 obj.address.country = 'India' //there will be spread only at one level thus change will be display in both objects
