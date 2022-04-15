0cf4205c37b45484cd28853b7fe422d06499fc35
Aditi9243 <aditi10950@gmail.com>
2022-04-15 23:41:36 +0530

    counter

--- prerequisite/class.js
+++ prerequisite/class.js
@@ -1,27 +1,39 @@
-class person {
+class Person {
     constructor(name, age) {
-        (this.name = name), (this.age = age);
+      (this.name = name), (this.age = age);
     }
-}
-
-class teacher extends person {
-    constructor(name, age, classStrength) {
-        super(name, age);
-        this.classStrength = classStrength;
+  
+    welcome(){
+       console.log("hello "+ this.name)
     }
-}
-
-class student extends person {
+  }
+  
+  class Teacher extends Person {
+    constructor(name , age ,classStrength) {
+       super(name , age)
+      this.classStrength = classStrength;
+    }
+  }
+  
+  class Student extends Person {
     constructor(name, age, cgpa) {
-        super(name, age);
-        this.cgpa = cgpa;
+      super(name, age);
+      this.cgpa = cgpa;
     }
-}
-
-let person1 = new person("Adam", 25);
-let teacher1 = new teacher("Steve", 40, 100);
-let student1 = new student("Mark", 21, 8.0);
-
-console.log(person1);
-console.log(teacher1);
-console.log(student1);
+  
+    hello(){
+           super.welcome()
+    }
+  }
+  
+  let person1 = new Person("Adam", 25);
+  let teacher1 = new Teacher("Steve", 40, 100);
+  let student1 = new Student("Mark", 21, 8.0);
+  
+  // console.log(person1);
+  // console.log(teacher1);
+  // console.log(student1);
+  
+  
+  person1.welcome()
+  student1.hello()
